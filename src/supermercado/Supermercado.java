fb21864898e021a3b2e3fa0e2d5b81ec07bfa58c
José Olinda <joseolindad@yahoo.com.br>
2019-10-08 08:57:46 -0300

    Finalizado cadastro de produtos

--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -1,9 +1,41 @@
 package supermercado;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Supermercado {
 
     public static void main(String[] args) {
+        // Cadastro de produtos
+        ArrayList<Produto> produtos = cadastrarProdutos();
+    }
+    
+    public static ArrayList<Produto> cadastrarProdutos() {
+       ArrayList listaDeProdutos = new ArrayList<Produto>(); 
+       System.out.println("### TELA DE CADASTRO DE PRODUTOS ###");
+       Scanner entrada = new Scanner(System.in);
+       boolean parar = false;
+       while (!parar) {
+        Produto novoP = new Produto();
         
+        System.out.println("Nome do produto:");
+        novoP.setNome(entrada.nextLine());
+        
+        System.out.println("Marca do produto:");
+        novoP.setMarca(entrada.nextLine());
+        
+        System.out.println("Preço do produto:");
+        novoP.setPreco(entrada.nextDouble());
+        
+        listaDeProdutos.add(novoP);
+        System.out.println("Produto cadastrado.\n\n\n");
+        System.out.println("Adicionar novo? [s/n]: ");
+        if (entrada.nextLine().equals("n") ) {
+            parar = true;
+        }
+       }
+       
+       return listaDeProdutos;
     }
     
 }
